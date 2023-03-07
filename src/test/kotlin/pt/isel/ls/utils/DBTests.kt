474de63323fa711590d8fe3d4a17371711846a57
manuel-48052 <80792945+manuel-48052@users.noreply.github.com>
2023-03-07 21:43:39 +0000

    insert_and_delete_test

--- src/test/kotlin/pt/isel/ls/utils/DBTests.kt
+++ src/test/kotlin/pt/isel/ls/utils/DBTests.kt
@@ -11,31 +11,50 @@ class DBTests {
         return  dataSource
     }
 
-    val dataSource = createDataSource()
+    lateinit var dataSource : PGSimpleDataSource
+
+
+    init{
+        dataSource= createDataSource()
+    }
 
     @Test
     fun select_test() {
         dataSource.getConnection().use {
             val stm = it.prepareStatement("select * from students")
             val rs = stm.executeQuery()
-
-
+            assert(rs != null)
         }
     }
-/*
+
     @Test
     fun insert_test() {
 
     }
-
+/*
     @Test
     fun insert_and_update_test() {
 
     }
-
+*/
     @Test
     fun insert_and_delete_test() {
 
+    dataSource.getConnection().use {
+        it.autoCommit = false
+        val stm = it.prepareStatement("insert into students(course, number, name) values (1, 12, 'mm')")
+        stm.execute()
+        val delt_stm = it.prepareStatement("delete from students where name = 'mm'")
+        delt_stm.execute()
+        val l_stm = it.prepareStatement("seltect * from students where name = 'mm'")
+        assertFailsWith<org.postgresql.util.PSQLException>{
+            val l_rs = stm.executeQuery()
+        }
+
+
+        it.rollback()
     }
-*/
+
+    }
+
 }
