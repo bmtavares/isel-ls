c8c6834d5de64fe5ff7d3ab4937678ca11bf81a5
manuel-48052 <80792945+manuel-48052@users.noreply.github.com>
2023-03-07 20:52:16 +0000

    added db tests

---
+++ src/test/kotlin/pt/isel/ls/utils/DBTests.kt
@@ -0,0 +1,29 @@
+package pt.isel.ls.utils
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class DBTests {
+    @Test
+    fun select_test() {
+
+    }
+
+    @Test
+    fun insert_test() {
+
+    }
+
+    @Test
+    fun insert_and_update_test() {
+
+    }
+
+    @Test
+    fun insert_and_delete_test() {
+
+    }
+
+}
