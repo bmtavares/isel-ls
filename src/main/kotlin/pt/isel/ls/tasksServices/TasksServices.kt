73624369f548f5d1d7c7e0495bdd15af115b83e3
manuel-48052 <80792945+manuel-48052@users.noreply.github.com>
2023-03-18 16:14:16 +0000

    deleted redundancy and updated the interface call of DB or File

--- src/main/kotlin/pt/isel/ls/tasksServices/TasksServices.kt
+++ src/main/kotlin/pt/isel/ls/tasksServices/TasksServices.kt
@@ -1,10 +1,12 @@
 package pt.isel.ls.tasksServices
 
+import pt.isel.ls.storage.DB.DBUser
 import pt.isel.ls.storage.File.FileUser
 import pt.isel.ls.storage.UserStorage
 
-class TasksServices() {
-    val userRepository: UserStorage = FileUser()
+class TasksServices(private val file : Boolean = false) {
+    private val userRepository: UserStorage = if(file){FileUser()}else{DBUser()}
+
     val users = ServiceUsers(userRepository)
 
 }
