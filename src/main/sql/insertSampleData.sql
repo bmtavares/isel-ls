4c93f8e87860288f187e1d58431da75fcb70684b
bmtavares <27867141+bmtavares@users.noreply.github.com>
2023-04-08 22:18:32 +0100

    Add script with sample data for PgSQL
    Moved createElement module to working folder

---
+++ src/main/sql/insertSampleData.sql
@@ -0,0 +1,58 @@
+begin; --transaction
+
+do $$ --anonymous pgsql scope for variables
+declare lastuserid integer;
+declare lastboardid integer;
+declare lastlistid integer;
+
+begin --anonymous scope
+
+insert into users(name, email) values
+    ('Beatriz', 'beatriz@example.org')
+    returning id into lastuserid;
+insert into userstokens(token, userId, creationDate) values
+    ('f52129ca-ccf1-42cc-a363-fdc89f71901b',lastuserid,'2023-02-23 18:30:00');
+
+insert into boards(name, description) values
+    ('Viagem na Europa', 'Reunir as ideias da viagem')
+    returning id into lastboardid;
+
+insert into usersboards(userid, boardid) values (lastuserid,lastboardid);
+
+insert into cards(name, description, duedate, listid, boardid) values
+    ('Viena','Austria',null,null,lastboardid),
+    ('Roma','Italia',null,null,lastboardid),
+    ('Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch','Pais de Gales',null,null,lastboardid);
+
+insert into lists(name, boardid) values
+    ('Reservas', lastboardid)
+    returning id into lastlistid;
+
+insert into cards(name, description, duedate, listid, boardid) values
+    ('Avião','Comprar os bilhetes para todos','2023-04-15 12:30:00',lastlistid,lastboardid),
+    ('Hotel','Reservar os quartos (para quantos?)','2023-05-01 12:00:00',lastlistid,lastboardid),
+    ('Transportes','De e para o aeroporto','2023-06-09 23:59:59',lastlistid,lastboardid);
+
+insert into lists(name, boardid) values
+    ('Sitos para comer', lastboardid),
+    ('Sitos para visitar', lastboardid);
+
+insert into users(name, email) values
+    ('Fatima', 'fatima@example.org')
+    returning id into lastuserid;
+insert into userstokens(token, userId, creationDate) values
+    ('6d061c83-707f-4143-9c66-5128a6c5ea63',lastuserid,'2023-02-23 18:31:00');
+
+insert into usersboards(userid, boardid) values (lastuserid,lastboardid);
+
+insert into users(name, email) values
+    ('Miguel', 'miguel@example.org')
+    returning id into lastuserid;
+insert into userstokens(token, userId, creationDate) values
+    ('95b36fe5-a100-462c-9123-dc310f92defc',lastuserid,'2023-02-23 18:31:30');
+
+insert into usersboards(userid, boardid) values (lastuserid,lastboardid);
+
+end $$; --anonymous scope
+
+commit; --transaction
