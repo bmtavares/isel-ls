c4912a4f96975e3453fa715e14888b41a5dc0fa6
bmtavares <27867141+bmtavares@users.noreply.github.com>
2023-05-21 20:12:23 +0100

    add generation helpers for bootstrap & html form

---
+++ static-content/generators/formGenerator.js
@@ -0,0 +1,35 @@
+import { form, div, label, input, button } from "./createElement.js";
+
+function generateField(domain, options) {
+  const name = `${domain}-${options.name.toLowerCase().split(" ").join("-")}`;
+  const inputOptions = { ...options };
+  inputOptions.id = name;
+  inputOptions.name = inputOptions.name.toLowerCase();
+  inputOptions.class = "form-control";
+  return div(
+    { class: "mb-3" },
+    label(options.name, { for: name, class: "col-form-label" }),
+    input(inputOptions)
+  );
+}
+
+export function generateFormModal(
+  domain,
+  onSubmit,
+  fields,
+  submitButtonText = "Submit"
+) {
+  return form(
+    { id: `${domain}-form`, events: { submit: onSubmit } },
+    ...fields.map((f) => generateField(domain, f)),
+    div(
+      { class: "modal-footer" },
+      button("Cancel", {
+        type: "button",
+        class: "btn btn-secondary",
+        "data-bs-dismiss": "modal",
+      }),
+      button(submitButtonText, { type: "submit", class: "btn btn-primary" })
+    )
+  );
+}
