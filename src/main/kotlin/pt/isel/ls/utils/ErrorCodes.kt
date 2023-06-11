bc28c50c4ce2d15ed831706eb07b61c0409fdf02
bmtavares <27867141+bmtavares@users.noreply.github.com>
2023-06-11 03:04:24 +0100

    Change exception on findByEmail

--- src/main/kotlin/pt/isel/ls/utils/ErrorCodes.kt
+++ src/main/kotlin/pt/isel/ls/utils/ErrorCodes.kt
@@ -2,5 +2,6 @@ package pt.isel.ls.utils
 
 enum class ErrorCodes(val code: Int, val message: String) {
     UNDEFINED(-1, "Undefined."),
-    AUTHENTICATION_CHALLENGE_FAILED(3000, "Authentication challenge failed.")
+    AUTHENTICATION_CHALLENGE_FAILED(3000, "Authentication challenge failed."),
+    NO_EMAIL_MATCH(3001, "Failed to get by email.")
 }
